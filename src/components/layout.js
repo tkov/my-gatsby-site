cff4f9b248ecc7581d1e81b9b6b93a4178775d62
Tim <tim.a.kovacs@gmail.com>
2021-08-15 09:18:07 -0400

    Finished Gatsby Tutorial Part 2

---
+++ src/components/layout.js
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText,
+} from './layout.module.css'
+
+const Layout = ({ pageTitle, children }) => {
+    return (
+        <div className={container}>
+            <title> { pageTitle } </title>
+            <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+                    <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                </ul>
+            </nav>
+            <main>
+                <h1 className={heading}> { pageTitle } </h1>
+                { children }
+            </main>
+        </div>
+    )
+}
+
+export default Layout;
+
+
+/*
+Note:
+
+    const Layout = ({ pageTitle, children }) => {...}
+    
+    is using destructuring of `props`. It is the shortform of:
+
+    const Layout = (props) => {
+        const pageTitle = props.pageTitle;
+        const children = props.children;
+        ...
+    }
+
+
+*/
---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+    return (
+        // <main>
+        //     <title>About Me</title>
+        //     <h1>About Me</h1>
+        //     <Link to="/">Back to Home</Link>
+        //     <p>Hi there! I'm the (not so proud) creator of this tutorial site, which I built with Gatsby.</p>
+        // </main>
+        <Layout pageTitle="About Me">
+            <p>Hi there! I'm the (not so proud) creator of this tutorial site, which I built with Gatsby.</p>
+        </Layout>
+    )
+
+}
+
+export default AboutPage;
